9a65b76bde9e6e951ad418779d38fbb563d41906
AmazingAsim <101821929+AmazingAsim@users.noreply.github.com>
2023-08-16 15:08:07 +0530

    added resume and bulid

--- src/components/Certificate.jsx
+++ src/components/Certificate.jsx
@@ -30,7 +30,7 @@ export default function Certificate() {
       {/* phase 1 */}
 
       <div class="modal fade" id="phase1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
+        <div class="modal-dialog modal-xl">
           <div class="modal-content">
             <div class="modal-header">
               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
@@ -44,7 +44,7 @@ export default function Certificate() {
       {/* phase 2 */}
 
       <div class="modal fade" id="phase2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
+        <div class="modal-dialog modal-xl">
           <div class="modal-content">
             <div class="modal-header">
               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
@@ -58,7 +58,7 @@ export default function Certificate() {
       {/* phase 3 */}
 
       <div class="modal fade" id="phase3" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
+        <div class="modal-dialog modal-xl">
           <div class="modal-content">
             <div class="modal-header">
               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
@@ -73,7 +73,7 @@ export default function Certificate() {
       {/* phase 3 */}
 
       <div class="modal fade" id="phase4" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
+        <div class="modal-dialog modal-xl">
           <div class="modal-content">
             <div class="modal-header">
               <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
